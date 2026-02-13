cf7efaf26e8bd77532d54d1f04667cedd4cd92d4
mn-sgm <matus17nemcik@gmail.com>
2026-02-13 10:20:50 +0100

    Add files via upload

---
+++ hra_strasidla_dafault.py
@@ -0,0 +1,28 @@
+#Strasidla
+from random import randint
+print("Strasidla")
+print()
+prezil_som = True
+skore = 0
+while prezil_som:
+    dvere_duch = randint(1, 3)
+    print("Trojo dverí pred tebou...")
+    print("Za jednými je duch.")
+    print("Ktoré dvere otvoríš?")
+    dvere = input("Zadaj 1, 2 alebo 3: ")
+    c_dveri = int(dvere)
+    
+    if __________  ==  ___________:  # podmienka: ak sa číslo dverí rovná dverám kde je duch
+        print()
+        print("STRASIDLOOO!!!")
+        prezil_som =                # tu uz nie je pravda, ze si prezil/a...
+        
+    else:
+        print()
+        print("Žiadny duch!")
+        print("Vstupuješ do ďalšej izby.")
+        print()
+        skore =                     # prezili sme, skore zvysime o 
+        
+print("Utekaj!")
+print("Koniec hry! Tvoje skore je", skore)
